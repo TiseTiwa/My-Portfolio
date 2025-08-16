3934e94386b837f5ec3e4219a6991de5ab2c1ffe
TiseTiwa <ismailtisetiwa@gmail.com>
2025-08-16 19:03:19 +0100

    Added bsc comp sci

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Client from "./pages/MainClient";
-import Developer from "./pages/Maindev";
+
 import Landing from "./pages/landing/Landing";
 import '@fortawesome/fontawesome-free/css/all.min.css';
 
@@ -11,8 +10,7 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Landing />} />
-        <Route path="/client" element={<Client />} />
-        <Route path="/developer" element={<Developer />} />
+       
       </Routes>
     </BrowserRouter>
   );
